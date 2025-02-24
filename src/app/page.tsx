4f52dff35e9953442ee4c03331aef6818ff8c63d
KomputerOSx <ramyarburhan26@gmail.com>
2025-02-24 07:29:31 +0000

    Removed a personal phone number
    fixed the footer on larger res screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,91 +18,99 @@ const App: React.FC = () => {
                 texts={["Pharmacy Tools", "Your Trusted Pharmacy Companion"]}
                 velocity={90}
                 className=""
+                parallaxClassName={"parallax"}
             />
-
-            <section className="section">
-                <div className="container">
-                    <h2 className="title is-2">Our Web Apps</h2>
-                    <div className="columns">
-                        {Object.entries({
-                            "Taper Calculator": {
-                                description:
-                                    "Calculate medication tapering schedules",
-                                link: "/taperCalculator",
-                            },
-                            "Contact Finder": {
-                                description: "Find pharmacy contacts easily",
-                                link: "/contacts",
-                            },
-                            "TBC...": {
-                                description: "More exciting tools coming soon!",
-                                link: "#",
-                            },
-                        }).map(([appName, appInfo]) => (
-                            <div className="column" key={appName}>
-                                <div className="card">
-                                    <div className="card-content">
-                                        <p className="title is-4">{appName}</p>
-                                        <p>{appInfo.description}</p>
+            <div className={"container"}>
+                <section className="section">
+                    <div className="container">
+                        <h2 className="title is-2">Our Web Apps</h2>
+                        <div className="columns">
+                            {Object.entries({
+                                "Taper Calculator": {
+                                    description:
+                                        "Calculate medication tapering schedules",
+                                    link: "/taperCalculator",
+                                },
+                                "Contact Finder": {
+                                    description:
+                                        "Find pharmacy contacts easily",
+                                    link: "/contacts",
+                                },
+                                "TBC...": {
+                                    description:
+                                        "More exciting tools coming soon!",
+                                    link: "#",
+                                },
+                            }).map(([appName, appInfo]) => (
+                                <div className="column" key={appName}>
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4">
+                                                {appName}
+                                            </p>
+                                            <p>{appInfo.description}</p>
+                                        </div>
+                                        <footer className="card-footer">
+                                            <a
+                                                href={appInfo.link}
+                                                className="card-footer-item animated-link"
+                                            >
+                                                Learn More
+                                            </a>
+                                        </footer>
                                     </div>
-                                    <footer className="card-footer">
-                                        <a
-                                            href={appInfo.link}
-                                            className="card-footer-item animated-link"
-                                        >
-                                            Learn More
-                                        </a>
-                                    </footer>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section className="section">
-                <div className="container">
-                    <h2 className="title is-2">Why Choose Pharmacy Tools?</h2>
-                    <div className="columns">
-                        {[
-                            {
-                                title: "User-Friendly",
-                                icon: faUser,
-                                color: "success",
-                                description:
-                                    "My tools are designed with the user in mind, making them easy to use.",
-                            },
-                            {
-                                title: "Speedy",
-                                icon: faFastForward,
-                                color: "info",
-                                description:
-                                    "Speed up your work, and stop repeating the same calculations.",
-                            },
-                            {
-                                title: "One Stop Shop",
-                                icon: faSync,
-                                color: "warning",
-                                description:
-                                    "Find all the tools you need in one place. No more searching.",
-                            },
-                        ].map((feature, index) => (
-                            <div className="column" key={index}>
-                                <div className="box">
-                                    <h3 className="title is-4">
-                                        <FontAwesomeIcon
-                                            icon={feature.icon}
-                                            className={`has-text-${feature.color} mr-2`}
-                                        />
-                                        {feature.title}
-                                    </h3>
-                                    <p>{feature.description}</p>
+                <section className="section">
+                    <div className="container">
+                        <h2 className="title is-2">
+                            Why Choose Pharmacy Tools?
+                        </h2>
+                        <div className="columns">
+                            {[
+                                {
+                                    title: "User-Friendly",
+                                    icon: faUser,
+                                    color: "success",
+                                    description:
+                                        "My tools are designed with the user in mind, making them easy to use.",
+                                },
+                                {
+                                    title: "Speedy",
+                                    icon: faFastForward,
+                                    color: "info",
+                                    description:
+                                        "Speed up your work, and stop repeating the same calculations.",
+                                },
+                                {
+                                    title: "One Stop Shop",
+                                    icon: faSync,
+                                    color: "warning",
+                                    description:
+                                        "Find all the tools you need in one place. No more searching.",
+                                },
+                            ].map((feature, index) => (
+                                <div className="column" key={index}>
+                                    <div className="box">
+                                        <h3 className="title is-4">
+                                            <FontAwesomeIcon
+                                                icon={feature.icon}
+                                                className={`has-text-${feature.color} mr-2`}
+                                            />
+                                            {feature.title}
+                                        </h3>
+                                        <p>{feature.description}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </>
     );
 };
